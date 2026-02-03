5d9921f4385322e5d8e3eb9e84e756cbfb978402
Bernd Herding <bernd.herding@external.biontech.com>
2026-02-04 00:39:12 +0100

    add improvements on .pre-commit-config.yaml et al

--- .support/post-process.sh
+++ .support/post-process.sh
@@ -4,11 +4,12 @@ awk '
     /^\| \[/ && match($0, /\]\(/) {
         prefix = substr($0, 1, RSTART-1)
         suffix = substr($0, RSTART)
-        gsub(/([^\\])_/, "\\1\\_")
-        # gsub(/_/, "\\_", prefix)
+        gsub(/\\_/, "\x01", prefix)    # protect already-escaped underscores
+        gsub(/_/, "\\_", prefix)       # escape remaining underscores
+        gsub(/\x01/, "\\_", prefix)    # restore protected ones
         $0 = prefix suffix
     }
     {print}
 ' README.new > README.md
 
-rm README.new
+# rm README.new
