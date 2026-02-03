29e59aa66a07025a7e69997ddf5e439f7cbcbb5c
Bernd Herding <bernd.herding@external.biontech.com>
2026-02-03 18:00:13 +0100

    add improvements on .pre-commit-config.yaml et al

--- site/sg.tf
+++ site/sg.tf
@@ -1,20 +1,16 @@
+# trivy:ignore:AVD-AWS-0099 (LOW): Security group explicitly uses the default description.
+# trivy:ignore:AVD-AWS-0104 (CRITICAL): Security group rule allows unrestricted egress to any IP address.
 resource "aws_security_group" "this" {
   region = var.site.region
   name   = "${var.name_tag}-${var.site.name}"
   vpc_id = aws_vpc.this.id
-
-  # ingress {
-  #   from_port = 0
-  #   to_port   = 0
-  #   protocol  = "-1"
-  #   self      = true
-  # }
 
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = [var.site.cidr, var.peer.cidr]
+    description = "allow ingress on all ports from peers"
   }
 
   egress {
@@ -22,6 +18,7 @@ resource "aws_security_group" "this" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "allow egress to everywhere"
   }
 
   tags = {
